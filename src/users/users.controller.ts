45ddcae71d56c01cce5602e8ae259f23d46ac641
Muhammad Huzaifa <mhuzaifa@nisum.com>
2022-07-26 16:54:06 +0500

    prisma configuration added

---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller()
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get('user')
+  async getUsers() {
+    return await this.userService.getUsers();
+  }
+}
